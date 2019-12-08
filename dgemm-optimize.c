095648edea4fef84c14840aa831895f0cff97e9f
Byron Garcia <byrongarcia@Byrons-MBP.attlocal.net>
2019-12-07 16:11:26 -0800

    Added three empty methods

--- dgemm-optimize.c
+++ dgemm-optimize.c
@@ -1,5 +1,24 @@
+/*
+The project requires a max of three methods which optimize
+on the naive solution. Two methods will be commented out at
+all times to allow for the testing to work. Otherwise it
+would not run since they will all be called dgemm, and have
+the same arguments.
+*/
+
+void dgemm( int m, int n, float *A, float *C )
+{
+// FILL-IN
+}
+
+/*
+void dgemm( int m, int n, float *A, float *C )
+{
+// FILL-IN  
+}
+
 void dgemm( int m, int n, float *A, float *C )
 {
 // FILL-IN 
-  
 }
+*/
